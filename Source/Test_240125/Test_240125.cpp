a8a556258ce01462b9033a64e6e09dba987728e1
charlieharrison08 <charlieharrison08@live.co.uk>
2025-01-24 17:23:39 +0000

    01 - Start of project

---
+++ Source/Test_240125/Test_240125.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Test_240125.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test_240125, "Test_240125" );
